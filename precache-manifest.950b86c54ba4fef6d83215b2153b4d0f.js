04ea1ebbf0de556830b97871280b804e9d304de3
Oleh <olkozhushko@gmail.com>
2019-06-19 15:01:30 +0300

    Updates

---
+++ precache-manifest.950b86c54ba4fef6d83215b2153b4d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b735368870613bc3dfe739205dee80",
+    "url": "/easy-peasy-todo/index.html"
+  },
+  {
+    "revision": "b156385d15d3212d148f",
+    "url": "/easy-peasy-todo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "259fa776b1bad3d651ca",
+    "url": "/easy-peasy-todo/static/css/main.b068f3b0.chunk.css"
+  },
+  {
+    "revision": "b156385d15d3212d148f",
+    "url": "/easy-peasy-todo/static/js/2.7299655a.chunk.js"
+  },
+  {
+    "revision": "259fa776b1bad3d651ca",
+    "url": "/easy-peasy-todo/static/js/main.34341c40.chunk.js"
+  },
+  {
+    "revision": "83fa7769a8fc886f6caa",
+    "url": "/easy-peasy-todo/static/js/runtime~main.1f7a8a4b.js"
+  }
+]);
